4b28fdcf32140ecdc30c5179a8beffae42b80a9a
iamNuttarika <67149449+iamNuttarika@users.noreply.github.com>
2020-07-08 17:09:29 +0800

    New Commit

---
+++ src/java/sg/edu/nyp/RegisterServlet.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package sg.edu.nyp;
+import java.io.Serializable;
+import javax.ejb.Stateless;
+
+@Stateless
+public class RegisterServlet{
+   
+    public static final long serialVersionUID = -1L;
+    
+ 
+    
+    }
+
+   
+    
+
