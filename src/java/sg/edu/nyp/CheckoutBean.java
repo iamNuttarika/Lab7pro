3a473411209a284200e196b9ee2537740c27ed0e
liang than <liang.than.gm@gmail.com>
2020-07-08 17:42:52 +0800

    checkout b e a n

---
+++ src/java/sg/edu/nyp/CheckoutBean.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package sg.edu.nyp;
+import javax.ejb.Stateless;
+@Stateless
+public class CheckoutBean {
+    public boolean validate(String cc)
+    {
+        
+        return false;
+    }
+}
