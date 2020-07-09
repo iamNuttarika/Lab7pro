8b3737c08f6938e9cebaa2756542c4faa31f6872
hn0598 <hemantnair0598@gmail.com>
2020-07-09 08:31:03 +0800

    Shopping cart module skeleton added.

--- src/java/sg/edu/nyp/Cart.java
+++ src/java/sg/edu/nyp/Cart.java
@@ -4,12 +4,76 @@
  * and open the template in the editor.
  */
 
-package sg.edu.nyp;
-
 /**
  *
  * @author heman
  */
-public class Cart {
+
+package sg.edu.nyp;
+
+
+import java.io.Serializable;
+
+
+public class Cart implements Serializable {
+    public static final long serialVersionUID = -1L;
+    private String itemDescription;
+    private String item;
+    private Integer quantity;
+    private Integer itemId;
+    private Integer categoryId;
+    private Float cost;
+   
+    public Cart() {
+    }
+
+    public String getItemDescription() {
+        return itemDescription;
+    }
+
+    public void setItemDescription(String itemDescription) {
+        this.itemDescription = itemDescription;
+    }
+ public String getItem() {
+        return item;
+    }
+
+    public void setItem(String item) {
+        this.item = item;
+    }
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+
+    public Integer getCategoryId() {
+        return categoryId;
+    }
+    
+     public void setCategoryID(Integer categoryId) {
+        this.categoryId = categoryId;
+    }
+
+    public Integer getItemId() {
+        return itemId;
+    }
+
+    public void setItemId(Integer itemId) {
+        this.itemId = itemId;
+    }
+
+    public Float getCost() {
+        return cost;
+    }
+
+    public void setCost(Float cost) {
+        this.cost = cost;
+    }
+
+   
+    
     
 }
