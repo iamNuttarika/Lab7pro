c895caf7bdc931a6a7532b9b6edfda316316a73a
iamNuttarika <67149449+iamNuttarika@users.noreply.github.com>
2020-07-16 09:45:30 +0800

    Hilabel test

--- src/java/sg/edu/nyp/RegisterRe.java
+++ src/java/sg/edu/nyp/RegisterRe.java
@@ -3,17 +3,85 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package sg.edu.nyp;
-
-import javax.ejb.Stateless;
 
 /**
  *
- * @author PNC
+ *
  */
+import javax.ejb.Stateless;
+
 @Stateless
 public class RegisterRe {
+
+    private int custId;
+    private String name;
+    private String email;
+    private String address;
+    private String postal;
+    private int phNum;
+    private String password;
     
-        public static final long serialVersionUID = -1L;
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    
+    
+    public RegisterRe() {
+    }
+
+
+    public void setCustId(int custId) {
+        this.custId = custId;
+    }
+
+    public int getCustId() {
+        return custId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPostal() {
+        return postal;
+    }
+
+    public void setPostal(String postal) {
+        this.postal = postal;
+    }
+
+    public int getPhNum() {
+        return phNum;
+    }
+
+    public void setPhNum(int phNum) {
+        this.phNum = phNum;
+    }
+
 }
--- web/Register.jsp
+++ web/Register.jsp
@@ -9,24 +9,24 @@ and open the template in the editor.
         <title>Register Page</title>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-      
+
     </head>
     <body>
-        
+
         <b>Please Register!</b><br/><br>
         <form name="Register" action="validate" method="post" onsubmit="return checkPass()" >
-        Name: <input type="text" name="name" required/><br/>
-        Email Address: <input type="email" name="email" required/><br/>
-        
-        Address: <input type="text" name="address1" required/><br/>
-                  
-        Postal Code : <input type="text" pattern="^[0-9]{6}" title="Please enter a 6-digit postal code "  name="postal" required/><br/>
-        
-        Mobile Number :<input type="tel" pattern="^[6,8,9]{1}[0-9]{7}" name="phNum" required/><br/>
-     
+            Name: <input type="text" name="name" required/><br/>
+            Email Address: <input type="email" name="email" required/><br/>
 
-        
-        <input type="submit" value="Register"/>
-    </form>
+            Address: <input type="text" name="address1" required/><br/>
+
+            Postal Code : <input type="text" pattern="^[0-9]{6}" title="Please enter a 6-digit postal code "  name="postal" required/><br/>
+
+            Mobile Number :<input type="tel" pattern="^[6,8,9]{1}[0-9]{7}" name="phNum" required/><br/>
+
+            Password :<input type="password" id="password" name="password" required/><br/>
+
+            <input type="submit" value="Register"/>
+        </form>
     </body>
 </html>
